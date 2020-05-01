9d220600a7b1b9e2f14552d81e529ddad01334f7
LuisEduardoSandoval <55498145+LuisEduardoSandoval@users.noreply.github.com>
2020-04-30 23:27:00 -0600

    Update Memento.java

--- SoftwareOOP/src/main/java/AdventureSimulator/Memento.java
+++ SoftwareOOP/src/main/java/AdventureSimulator/Memento.java
@@ -6,23 +6,37 @@ public class Memento	{
 
 	private int xp;
 
-	public Memento(Classes player)	{
+        
+        private int enemyhp;
+	public Memento(Classes player, Enemies Enemy)	
+        {
 		
 		this.hp = player.getPlayerhp();
 
 		this.xp = player.getXp();
+                
+                this.enemyhp = Enemy.getEnemyHP();
 	}
+
+    Memento(int savedhp, int savedxp, int enemyHP) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 	
-	public String getHP()	{
+	public int getHP()	
+        {
 		
 		return this.hp;
 
 	}
 
-	public String getXP()	{
+	public int getXP()	{
 
 		return this.xp;
 	}
+        public int getEnemyHP()
+        {
+            return this.enemyhp;
+        }
 
 
 }
